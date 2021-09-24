c18270b83b6e9c2d44a21d984263ff5502954d06
salahudeentope <salahudeentope@gmail.com>
2021-09-24 15:50:38 +0100

    Handle edge cases of missing image thumbnail and joining authors names

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -22,7 +22,7 @@ class Shelf extends React.Component {
                   <div className="book">
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 193, 
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                        backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})`}}></div>
                       <div className="book-shelf-changer">
                         <select value={book.shelf} onChange={e => this.props.changeShelf(book, e.target.value)} >
                           <option value="move" disabled>Move to...</option>
@@ -34,7 +34,7 @@ class Shelf extends React.Component {
                       </div>
                     </div>
                     <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
+                    <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
                   </div>
                 </li>)}
               </ol>
